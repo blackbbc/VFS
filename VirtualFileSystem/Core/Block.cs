fb6ce6bed0b12e41ba9766897d2335141258d014
Sweet <ly505968815@gmail.com>
2015-06-21 14:54:26 +0800

    lalala

---
+++ VirtualFileSystem/Core/Block.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualFileSystem.Core
+{
+    class Block
+    {
+        public char[] data;
+        public Block[] block;
+
+        public Block() { }
+
+        public Block(Boolean flag)
+        {
+            if (flag)
+            {
+                data = new char[1024];
+            }
+            else
+            {
+                block = new Block[512];
+            }
+        }
+        
+    }
+}
--- VirtualFileSystem/Form1.cs
+++ VirtualFileSystem/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using VirtualFileSystem.Core;
+
 namespace VirtualFileSystem
 {
     public partial class Form1 : Form
@@ -15,6 +17,19 @@ namespace VirtualFileSystem
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Block block1 = new Block();
+
+            Block block2 = block1;
+
+            block2.data[0] = 'a';
+
+            MessageBox.Show(block2.data[1].ToString());
+
+            
         }
     }
 }
